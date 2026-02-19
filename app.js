553e2652f99fce50463ffb43a1764f43ed2a1044
vanaiyalini <kyalini2001@gmail.com>
2026-02-19 13:17:40 +0530

    create a module file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const myModule = require('./my-module');
+
 
 fs.readFile('file.txt', 'utf8', function (err, data) {
   if (err) throw err;
@@ -9,3 +11,5 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err;
   console.log('File saved!');
 });
+
+console.log(myModule.myFunction());
